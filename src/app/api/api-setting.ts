595a01c94b0199f880252106046b955dbfe7c1f9
winswe <mws.mdm@gmail.com>
2023-05-22 10:47:09 +0630

    a

--- src/app/api/api-setting.ts
+++ src/app/api/api-setting.ts
@@ -1,6 +1,6 @@
 export class ApiSetting {
     public static get UserApiEndPoint() {
-        return 'http://192.168.100.4:8079';
+        return 'http://localhost:8079';
     }
 
     public static get AccountApiEndPoint() {
